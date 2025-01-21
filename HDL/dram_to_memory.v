9e7827dd346ef93a270958b18068d826db3c5daf
Amirhossein Zarei <amirhozarei@gmail.com>
2025-01-21 17:46:00 -0500

    Changes to be committed: 	modified:   dram_to_memory.v

--- HDL/dram_to_memory.v
+++ HDL/dram_to_memory.v
@@ -43,30 +43,25 @@ module dram_to_memory
             accumulated_data <= {DATA_OUT_BITWIDTH{1'b0}};  // Reset accumulated data
             bit_count <= {$clog2(DATA_OUT_BITWIDTH){1'b0}};           // Reset bit count
             carry_over_bits <= {DATA_IN_BITWIDTH{1'b0}};     // Reset carry-over bits
-            bram_write_enable <= 0;      // Disable BRAM write
+            memory_write_enable <= 0;      // Disable BRAM write
             carry_flag <= 0;
-            carry_over_number_bits <= 0;
-        end else if (data_valid) begin
+            carry_over_number_bits <= {DATA_IN_BITWIDTH{1'b0}};
+        end else if (data_valid_i) begin
 
-            if ((bit_count == 0) && carry_flag == 1 )begin
-                accumulated_data <= {accumulated_data[DATA_OUT_BITWIDTH - carry_over_number_bits - 1:0], carry_over_bits [carry_over_number_bits - 1 : 0]};
-                bit_count <= bit_count + carry_over_number_bits;
+            if (bit_count + DATA_IN_BITWIDTH + carry_over_number_bits < DATA_OUT_BITWIDTH)begin
+                accumulated_data <= {accumulated_data[DATA_OUT_BITWIDTH - carry_over_number_bits - DATA_IN_BITWIDTH - 1:0], carry_over_bits [carry_over_number_bits - 1 : 0], data_in_i};
+                bit_count <= bit_count + DATA_IN_BITWIDTH + carry_over_number_bits;
                 carry_flag <= 0;
+                carry_over_number_bits <= {DATA_IN_BITWIDTH{1'b0}};  
                 carry_over_bits <= {DATA_IN_BITWIDTH{1'b0}};
-            end else if (bit_count + DATA_IN_BITWIDTH < DATA_OUT_BITWIDTH) begin
+            end else if (bit_count < DATA_OUT_BITWIDTH) begin
  
-                accumulated_data <= {accumulated_data[DATA_OUT_BITWIDTH - 1 - DATA_IN_BITWIDTH :0], data_in};
-                bit_count <= bit_count + DATA_IN_BITWIDTH ;
-            end else begin
-                
-                if (bit_count < DATA_OUT_BITWIDTH) begin
-                   
-                    accumulated_data <= {accumulated_data[bit_count : 0], data_in[DATA_IN_BITWIDTH -1 : DATA_IN_BITWIDTH - (DATA_OUT_BITWIDTH - bit_count - 1)]};
-                    carry_over_bits [DATA_IN_BITWIDTH - (DATA_OUT_BITWIDTH - bit_count - 1) - 1 : 0] <= data_in[DATA_IN_BITWIDTH - (DATA_OUT_BITWIDTH - bit_count - 1) - 1 : 0];  // Store remaining bits
-                    carry_over_number_bits <= DATA_IN_BITWIDTH- (DATA_OUT_BITWIDTH - bit_count - 1);
-                    bit_count <= bit_count + DATA_OUT_BITWIDTH - bit_count - 1;
-                    carry_flag <= 1;
-                end
+                accumulated_data <= {accumulated_data[bit_count : 0], data_in[DATA_IN_BITWIDTH -1 : DATA_IN_BITWIDTH - (DATA_OUT_BITWIDTH - bit_count - 1)]};
+                carry_over_bits [DATA_IN_BITWIDTH - (DATA_OUT_BITWIDTH - bit_count - 1) - 1 : 0] <= data_in_i[DATA_IN_BITWIDTH - (DATA_OUT_BITWIDTH - bit_count - 1) - 1 : 0];  // Store remaining bits
+                carry_over_number_bits <= DATA_IN_BITWIDTH- (DATA_OUT_BITWIDTH - bit_count - 1);
+                bit_count <= bit_count + DATA_OUT_BITWIDTH - bit_count - 1;
+                carry_flag <= 1;
+            
             end
             
             // If DATA_OUT_BITWIDTH bits are accumulated, store them to BRAM
